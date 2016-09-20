7d2b200e89b672d8bf02deb73e245ef8c79da63f
Cyber-Mac-17 <cyber-mac-17@MacBook-Pro-17.local>
2016-09-20 13:02:37 +0700

    update project

--- src/app/main/apps/employee/employee.controller.js
+++ src/app/main/apps/employee/employee.controller.js
@@ -10,6 +10,7 @@
         $scope.colors = ['blue-bg', 'blue-grey-bg', 'orange-bg', 'pink-bg', 'purple-bg'];
         $scope.positions = [];
         $scope._positionSelected = "";
+        $scope._selectedemp = {};
 
         $scope.positionSelected = function(position) {
             $scope._positionSelected = position;
@@ -17,32 +18,123 @@
 
         $scope.getEmployeeList = function() {
             $scope._getEmployeeList = [{
-                "_id":"1",
-                "fname":"Dook",
-                "lname":"Apple",
-                "position":"SystemAnalyst"
-            },
-            {
-                "_id":"2",
-                "fname":"Dook",
-                "lname":"Apple",
-                "position":"Developer"
-            },
-            {
-                "_id":"3",
-                "fname":"Dook",
-                "lname":"Apple",
-                "position":"Developer"
+                "_id": "57db700ae81c7001da81d05a",
+                "__v": 0,
+                "Updated": "2016-09-16T06:10:30.384Z",
+                "Created": "2016-09-16T04:07:38.217Z",
+                "SilpInfo": [],
+                "DeductionInfo": [],
+                "IncreaseInfo": [{
+                    "EndDateIn": "2016-09-16T17:00:00.000Z",
+                    "StartDateIn": "2016-09-07T17:00:00.000Z",
+                    "AmountIn": 23456,
+                    "DetailIn": "vv",
+                    "_id": "57db700ae81c7001da81d05a"
+                }, {
+                    "EndDateIn": "2016-09-10T17:00:00.000Z",
+                    "StartDateIn": "2016-09-09T17:00:00.000Z",
+                    "AmountIn": 45,
+                    "DetailIn": "rrr",
+                    "_id": "57db700ae81c7001da81d05a"
+                }],
+                "EvaluationInfo": {
+                    "DateConfirmedEV": "2016-09-02T17:00:00.000Z",
+                    "SumP5EV": 0,
+                    "SumP4EV": 0,
+                    "SumP3EV": 0,
+                    "SumP2EV": 0,
+                    "SumP1EV": 0,
+                    "TotalScoreEV": null,
+                    "PercentEV": null
+                },
+                "RegardInfo": {
+                    "StartDateRI": "2016-09-02T17:00:00.000Z",
+                    "BenefitsInfo": []
+                },
+                "LanguageInfo": [],
+                "WorkExperienceInfo": [],
+                "EducationInfo": [],
+                "FamilyInfo": [],
+                "PersonalInfo": {
+                    "Age": 60,
+                    "Sex": "Male",
+                    "Position": "Developer",
+                    "Email": "toonelicious@gmail.com",
+                    "Nationality": "ไทย",
+                    "LastNameEN": "จงกลางธวัชกิจ",
+                    "FirstNameEN": "จักรกฤช822"
+                }
+            }, {
+                "_id": "57db70c1e81c7001da81d05b",
+                "__v": 0,
+                "Created": "2016-09-16T04:10:41.688Z",
+                "SilpInfo": [],
+                "DeductionInfo": [],
+                "IncreaseInfo": [],
+                "RegardInfo": {
+                    "BenefitsInfo": []
+                },
+                "LanguageInfo": [],
+                "WorkExperienceInfo": [],
+                "EducationInfo": [],
+                "FamilyInfo": [],
+                "PersonalInfo": {
+                    "FirstNameEN": "จักรกฤช6",
+                    "LastNameEN": "จงกลางธวัชกิจ",
+                    "Nationality": "ไทย",
+                    "Email": "toonelicious@gmail.com"
+                }
+            }, {
+                "_id": "57db8969e81c7001da81d05c",
+                "__v": 0,
+                "Created": "2016-09-16T05:55:53.509Z",
+                "SilpInfo": [],
+                "DeductionInfo": [],
+                "IncreaseInfo": [],
+                "RegardInfo": {
+                    "BenefitsInfo": []
+                },
+                "LanguageInfo": [],
+                "WorkExperienceInfo": [],
+                "EducationInfo": [],
+                "FamilyInfo": [],
+                "PersonalInfo": {
+                    "FirstNameEN": "mm",
+                    "LastNameEN": "nn"
+                }
+            }, {
+                "_id": "57db8a34e81c7001da81d05d",
+                "__v": 0,
+                "Created": "2016-09-16T05:59:16.043Z",
+                "SilpInfo": [],
+                "DeductionInfo": [],
+                "IncreaseInfo": [],
+                "RegardInfo": {
+                    "BenefitsInfo": []
+                },
+                "LanguageInfo": [],
+                "WorkExperienceInfo": [],
+                "EducationInfo": [],
+                "FamilyInfo": [],
+                "PersonalInfo": {
+                    "FirstNameEN": "hh",
+                    "LastNameEN": "hh",
+                    "Position": "Developer"
+                }
             }];
 
-            $scope._getEmployeeList.forEach(function(entry){
-                console.log(entry.position);
-                var isNew = $scope.positions.indexOf(entry.position) == -1;
+            $scope._getEmployeeList.forEach(function(entry) {
+                var isNew = $scope.positions.indexOf(entry.PersonalInfo.Position) == -1;
                 console.log(isNew);
-                if(isNew){
-                    $scope.positions.push(entry.position);
+                if (isNew) {
+                    $scope.positions.push(entry.PersonalInfo.Position);
                 }
             });
+
+            $scope.selectedemp = function(emp) {
+                console.log(emp);
+                $scope._selectedemp = emp;
+            }
 
 
         }
